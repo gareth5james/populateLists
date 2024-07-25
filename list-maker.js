ab144bb9e71cbdac5ec448d9ee3b1cf9949a0d4e
Gareth <gareth.james@force24.co.uk>
2024-07-25 16:11:01 +0100

    first setup

---
+++ list-maker.js
@@ -0,0 +1,8 @@
+function populateList(input) {
+    //write create and populate list elements here
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./output.json')
+      //pass data to function here
+});
